a3e5141237f669b7643bff7a24d83c769ce36132
Edouard-Legoupil <edouard.legoupil@gmail.com>
2017-09-10 17:31:15 +0300

    check on label matching

--- R/kobo_label.R
+++ R/kobo_label.R
@@ -27,11 +27,16 @@
 
 kobo_label <- function(datalabel, dico) {
   ### First we provide attribute label to variable name
-  #datalabel <- individual_biodata3
+  #datalabel <- data
   data.label <- as.data.frame(names(datalabel))
   names(data.label)[1] <- "fullname"
   data.label <- join (x=data.label, y=dico, by="fullname", type="left" )
   for (i in 1:nrow(data.label)) { attributes(datalabel)$variable.labels[ i] <- as.character(data.label[ i, c("label")]) }
-
+  test <- data.label[ !(is.na(data.label$name)), ]
+  if (nrow(data.label) > nrow(test)) {
+    cat (paste0("you have ",nrow(data.label), " variables in you frame but only ",nrow(test) ," were relabelle.\n"))
+    cat(" You may double check that the form and the data are matching \n")
+    cat("Double check as well that you did download the data with the correct header (i.e. full path with point delimiters) \n")
+  } else { cat ("All variables were mapped. great \n")}
   return(datalabel)
 }

b39affdf9002bcd6b1ca6020450b5aa9200ad794
Edouard-Legoupil <edouard.legoupil@gmail.com>
2017-10-26 14:36:50 +0300

    adding viridis package

--- inst/script/0-packages.R
+++ inst/script/0-packages.R
@@ -8,6 +8,7 @@ packages <- c(
   ### Packages for Premodeling Stage
 
   ## Data Manipulation
+  "viridis",
   "lubridate",
   "date","gdata","zoo", ## playing with date
   "dplyr",  "data.table", "doBy","tidyr", ## Data manipulation
@@ -15,24 +16,24 @@ packages <- c(
   "stringr","stringdist","stringi", ## string manipulation
 
   ## Missing value imputation
- # "missForest",  "missMDA", "Amelia",
+  # "missForest",  "missMDA", "Amelia",
 
 
   ## Outlier Detection
- # "outliers",  "evir",
+  # "outliers",  "evir",
 
   ## Feature Selection
- #"features",  "RRF",
- # "Boruta", # wrapper for feature selection algorythm
+  #"features",  "RRF",
+  # "Boruta", # wrapper for feature selection algorythm
 
   ## Dimension Reduction
- # "CCP", # Dimension Reduction
- # "FactoMineR", "ade4",  ## multivariate analysis - MCA
+  # "CCP", # Dimension Reduction
+  # "FactoMineR", "ade4",  ## multivariate analysis - MCA
 
 
   ##### Packages for Visualisation
   #"lattice", # Visualisation
- "graphics",
+  "graphics",
   "ggplot2", ## advanced graphics
   "ggseas", ## seasonal adjustemnt with GGplot2
   "ggrepel", ## getting nice labels in ggplot2
@@ -57,84 +58,84 @@ packages <- c(
   "ggmap", ## get background from webmapping API
   "raster","cartography", ## packages used for the maps --
   "classInt",  ## used for univariate classification
- # "deldir", # delaunay triangulation & Voronoi
- # "viridis", # Default Color Maps from 'matplotlib'
- # "fields", ## Tools for Spatial Data
+  # "deldir", # delaunay triangulation & Voronoi
+  # "viridis", # Default Color Maps from 'matplotlib'
+  # "fields", ## Tools for Spatial Data
 
   ##################################################################
   ### Packages for Modeling Stage
 
 
-#  "Hmisc", # generate a detailled describtion of a given dataset
-#  "gbm", # Generalized Boosted Regression Models
-#  "car", ## ## Companion to Applied Regression
-#  "rminer", "CORElearn",  # ordinal Regression
-#  "caret", # Gradient Boosting & AdaBoost
-#  "bigRR",  ## Classification
+  #  "Hmisc", # generate a detailled describtion of a given dataset
+  #  "gbm", # Generalized Boosted Regression Models
+  #  "car", ## ## Companion to Applied Regression
+  #  "rminer", "CORElearn",  # ordinal Regression
+  #  "caret", # Gradient Boosting & AdaBoost
+  #  "bigRR",  ## Classification
 
 
 
-#  "e1071", #SVM (Support Vector Machine)
-#  "knncat", # KNN (K- Nearest Neighbors)
-#  "randomForest", # randomForest
-#  "stats", # Dimensionality Reduction Algorithms princomp
+  #  "e1071", #SVM (Support Vector Machine)
+  #  "knncat", # KNN (K- Nearest Neighbors)
+  #  "randomForest", # randomForest
+  #  "stats", # Dimensionality Reduction Algorithms princomp
   ## Time Series
-#  "forecast", "ltsa",
+  #  "forecast", "ltsa",
 
   # survival analysis
-#  "survival", "BaSTA",
-#  "pastecs", #Analysis of Space-Time Ecological Series
+  #  "survival", "BaSTA",
+  #  "pastecs", #Analysis of Space-Time Ecological Series
 
   # Lasso and Elastic-Net Regularized Generalized Linear Models
-#  "glmnet",
-#  "lme4", # Linear Mixed-Effects Models
+  #  "glmnet",
+  #  "lme4", # Linear Mixed-Effects Models
 
-#  "MASS",
-#  "VGAM", #Vector Generalized Linear and Additive Models
-#  "aod", ## Analysis of Overdispersed Data
+  #  "MASS",
+  #  "VGAM", #Vector Generalized Linear and Additive Models
+  #  "aod", ## Analysis of Overdispersed Data
 
   ## Cluster analysis
-#  "cluster", "cba", "Rankcluster",
+  #  "cluster", "cba", "Rankcluster",
 
   ##################################################################
   ### Packages for Post Modeling Stage
 
-#  "lmtest", # Testing Linear Regression Models
+  #  "lmtest", # Testing Linear Regression Models
 
-#  "gvlma", #Global Validation of Linear Models Assumptions
+  #  "gvlma", #Global Validation of Linear Models Assumptions
 
-#  "lsmeans", "comparison", #general Model Validation
-#  "regtest", "ACD", #Regression validation
+  #  "lsmeans", "comparison", #general Model Validation
+  #  "regtest", "ACD", #Regression validation
 
-#  "binomTools","Daim", ## classification validation
-#  "clusteval","sigclust", ## Clustering valisation
+  #  "binomTools","Daim", ## classification validation
+  #  "clusteval","sigclust", ## Clustering valisation
 
-#  "pROC","timeROC", # ROC Analysis
+  #  "pROC","timeROC", # ROC Analysis
 
   ## Recursive Partitioning and Regression Trees
-#  "rpart", "rpart.plot",
+  #  "rpart", "rpart.plot",
 
   ##################################################################
   ### Packages for Survey data management
-#  "sampling", ## Survey Sampling
+  #  "sampling", ## Survey Sampling
   "survey",  ##Analysis of Complex Survey Samples
 
   ##################################################################
   ### Other Packages
 
 
-#  "psych", ## Procedures for Psychological, Psychometric, and Personality Research
+  #  "psych", ## Procedures for Psychological, Psychometric, and Personality Research
 
-#  "Benchmarking", #Benchmark and Frontier Analysis Using Data Envelopmenbt Aanalysis
+  #  "Benchmarking", #Benchmark and Frontier Analysis Using Data Envelopmenbt Aanalysis
 
-#  "pwr", # Power Analysis allows  to determine the sample size required to detect an effect of a given size with a given degree of confidence.
+  #  "pwr", # Power Analysis allows  to determine the sample size required to detect an effect of a given size with a given degree of confidence.
 
   ## text mining
-#  "tm", "twitteR" ,
-#  "wordcloud", #Word Clouds
-#  "LDAvis", # Interactive Visualization of Topic Models
+  #  "tm", "twitteR" ,
+  #  "wordcloud", #Word Clouds
+  #  "LDAvis", # Interactive Visualization of Topic Models
 
-#  "AER",  # Applied economtrics with R
+  #  "AER",  # Applied economtrics with R
 
   "formatR", #  used to format the code
 
@@ -142,9 +143,9 @@ packages <- c(
   "Rcpp", ## used to compile some pacjckages
 
   "foreign", ## read data from SPSS, SAS or Stata
-#  "sqldf", "RODBC",
+  #  "sqldf", "RODBC",
   #"RMongo",
-#  "RSQLite", ## Direct connection with databases
+  #  "RSQLite", ## Direct connection with databases
 
   #"rJava", "XLConnect", ## Read and write excel files
   "readxl", ## Read Excel files
@@ -155,9 +156,9 @@ packages <- c(
   "RCurl", ##used to download files from API -install CURL before and separately
   "devtools", # package used to load packages hosted in github --
 
-#  "gmailr", # Access gmail api
+  #  "gmailr", # Access gmail api
   "RGtk2",
-#  "rattle", ## GUI for data mining
+  #  "rattle", ## GUI for data mining
   ## used to generate reports
   "knitr", "pander", "xtable", "rmarkdown", "kableExtra","koRpus", "tables"
 )
@@ -181,7 +182,7 @@ library("rmarkdown")
 
 library("survey")
 library("ggrepel")
-
+library("viridis")
 #gpclibPermit()
 
 # loads packages into memory

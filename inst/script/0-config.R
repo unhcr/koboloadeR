724007f61d1a72431023e5a4d249c4515649d4f3
Edouard-Legoupil <edouard.legoupil@gmail.com>
2018-02-20 07:03:38 +0200

    revise the project configuration script

--- inst/script/0-config.R
+++ inst/script/0-config.R
@@ -23,22 +23,68 @@ path.to.data <- "data/data.csv"
 ## let's recode the variable of the dataset using short label - column 3 of my reviewed labels
 #names(data.or) <- datalabel[, 2]
 
+###### 2a. case of nested dataframe
+
+### We will set all configuration within a dataframe
+
+## 1st column: name is the name of the dataframe - per convention the first one is call household
+## 2nd column: path is the path to the csv file - extracted with ODK briefcase
+## 3rd column: sep is the type of the separator used in the header format - default is '.' - if not '.' it will be converted
+## 4rd column: framid is the name of the unique ID for the frame
+## 6th column:parent is the name of the parent frame
+## 7th column:parentid is the name of the corresponding ID to be used for the join in the parent frame
+## 8th column: suffix string used if nested frame header does not include full xml path
+
+frame1 <- c("name" = "",
+            "path" = "",
+            "sep" = "",
+            "framid" = "",
+            "parent" = "",
+            "parentid" = "",
+            "suffix" = ""  )
+
+frame2 <- c("name" = "",
+            "path" = "",
+            "sep" = "",
+            "framid" = "",
+            "parent" = "",
+            "parentid" = "",
+            "suffix" = ""  )
+
+frame3 <- c("name" = "",
+            "path" = "",
+            "sep" = "",
+            "framid" = "",
+            "parent" = "",
+            "parentid" = "",
+            "suffix" = ""  )
+
+# path.to.data <- rbind(t(frame1), t(frame2), t(frame3))
 
 #### 3. data cleaning *********************
-## Might need to be tweaked -- double check
-#path.to.data.update <- "data/datadelete.csv"
-## Might need to be tweaked -- double check
-#path.to.data.delete <- "data/datadelete.csv"
+cleaning.log <- "no"
+
+#############################
+### if cleaning.log <- "yes" please document the following
+## path.to.cleaning.script <- "code/cleanscript.R"
+##  path.to.data.update <- "data/datadelete.csv"
+##  path.to.data.delete <- "data/datadelete.csv"
 
 
 #### 4. sample weight *********************
-## Might need to be tweaked -- double check
-#path.to.weight <- "data/dataweight.csv"
+sample.weight <- "no"
 
+#############################
+### if sample.weight <- "yes" please document the following
+# path.to.weight <- "data/dataweight.csv"
+# id.to.join.weight <- "weight.id"
+# sampling.type <-  ## take numeric value 1:cluster sampling 2: stratified sampling
 
 #### 5. Indicator *********************
-## Might need to be tweaked -- double check
+indicator.calculation <- "no"
+
+#############################
+### if indicator.calculation <- "yes" please document the following
 #library(readxl)
 #indicator <- read_excel("data/form.xls", sheet = "indicator")
-
 

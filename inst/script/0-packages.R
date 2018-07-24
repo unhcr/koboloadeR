2dd196ef18812503480becfb042f0113ab90ccf8
Edouard-Legoupil <edouard.legoupil@gmail.com>
2018-07-24 16:39:09 +0300

    Impoved the script to install packages

--- inst/script/0-packages.R
+++ inst/script/0-packages.R
@@ -1,9 +1,44 @@
 ## A curated list of packages
 ## @edouard_lgp
+## additional inspiration form https://github.com/ThinkR-open/installR/blob/master/install-mini.R
+
+percent <- function(x, digits = 2, format = "f", ...) {
+  paste0(formatC(100 * x, format = format, digits = digits, ...), "%")
+}
+
+mon_print <- function(synth){
+  N <- nrow(synth)
+  if (is.null(N)) {  N <- 1
+    synth <- matrix(synth, nrow = 1) }
+  for (i in 1:N) {
+    tp <- synth[i,]
+    tp[1]  <- stringr::str_pad(tp[1], 17, side = "right")
+    cat(tp,"\n")
+  }
+}
+
+
+## Few options to set up
+try(setInternet2(use = TRUE),silent = TRUE)
+packs <- installed.packages()
+exc <- names(packs[,'Package'])
+local({r <- getOption("repos"); r["CRAN"] <- "https://cran.r-project.org/"; options(repos = r)})
+
+
+## Fixing automatically the "Unable to move temporary installation" R issue
+## https://stackoverflow.com/questions/47760372/non-interactive-editing-fixing-of-invisible-and-locked-function
+#trace(utils:::unpackPkgZip, edit=TRUE) # your edits
+#unpackPkg <- utils:::unpackPkgZip # copy function
+
+# save edited function to file Into C:\Users\myself\Documents\R\R-3.4.4\etc\Rprofile.SITE:
+#saveRDS(unpackPkg, file = "unpack.rds")
+#unpack <- readRDS(file = "C:/Users/myself/Documents/R/R-3.4.4/etc/unpack.rds")
+#utils::assignInNamespace("unpackPkgZip", unpack, ns = "utils") # overwrite default
+
+
 
 ##This should detect and install missing packages before loading them –
-packages <- c(
-
+packages <- unique(sort(c(
 
   ## Tydiverse #########
   # https://www.tidyverse.org
@@ -199,14 +234,68 @@ packages <- c(
   "kableExtra",
   "koRpus",
   "tables"
-)
+)))
 
-## identify packages not installed yet
-if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
-  install.packages(setdiff(packages, rownames(installed.packages())), dependencies = TRUE)
+vrai_liste <- packages
+try(vrai_liste <- unique(unlist(tools::package_dependencies(packages,recursive = TRUE))))
+
+
+for (elpa in packages) {
+  exc <- names(installed.packages()[,'Package'])
+  cat("Package Installation progress so far.. : ",
+      percent(sum(is.element(packages,exc))/length(packages), digits = 0)
+      ," - ",
+      percent(sum(is.element(vrai_liste,exc)) / length(vrai_liste), digits = 0)
+      ,"\n\n")
+  if (is.element(elpa,exc))  { cat(elpa," is already installed on your computer \n")  }
+  if (!is.element(elpa,exc)) { cat(elpa," will be installed on your computer  \n")
+    try(utils:::install.packages(elpa,dependencies = TRUE))
+    packs <- installed.packages()
+    exc <- names(packs[,'Package'])
+    if (is.element(elpa,exc)) { cat("   The installation went well for the package: ",elpa,"\n")
+    }
+  }
 }
 
-rm(packages)
+cat("\n\n\n ***Installation completed*** \n\n\n")
+
+
+packs <- installed.packages()
+exc <- names(packs[,'Package'])
+
+## adding the short description of the package
+packsdescp <- as.data.frame(packs)
+for (i in 1:nrow(packsdescp)) {
+  # i <- 1
+  packsname <- as.character(packsdescp[ i, c("Package") ])
+  #cat(packsname)
+  packsdescp[ i, c("Title") ] <- as.character(packageDescription(packsname, fields = c("Title")))
+}
+
+packsdescp <- unique(packsdescp[ , c("Package", "Title")])
+packsdescp$Title <- substr(packsdescp$Title,0, 60)
+
+synth <- as.data.frame(cbind(unique(packages),is.element(packages,exc)))
+names(synth)[1] <- "packages"
+synth <- merge( x = synth, y = packsdescp[ ,c("Package","Title")],by.x = "packages", by.y = "Package", all.x = TRUE)
+synth$packages <- as.character(synth$packages)
+manque <- as.matrix(synth[synth[,2] == "FALSE", c("packages", "Title")])
+synth <- as.matrix(synth[synth[,2] == "TRUE", c("packages", "Title")])
+
+cat("\n\n\n Below is the list of packages that have been now installed on your computer : \n\n")
+#cat(synth)
+try(mon_print(synth),silent = TRUE)
+
+cat("\n\n\nThe list below indicates the missing packages: \n\n")
+#cat(manque)
+try(mon_print(manque),silent = TRUE)
+
+if (nrow(manque) == 0) {cat("Perfect, there's no missing package \n\n\n")} else
+{cat(" You have missing package -> run again this script or try to install them one by one using the 'install' button under the 'packages' tab \n\n")}
+
+try(silent = TRUE,rm(packages,elpa,exc,packs,percent,mon_print,synth,manque,vrai_liste))
+
+
 
 # Now load packages into memory ####
 # Sys.getenv("R_LIBS_USER")
@@ -250,4 +339,6 @@ library("readxl")
 #library("xlsx")
 
 library("car")
+
+
 

e6242240c0477d10ad1d1ae219f683e563088067
ElliottMess <elliott.messeiller@gmail.com>
2019-01-12 07:16:45 -0600

    Updating kobo_label
    Updating kobo_label to rename not only the dataframe columns, but also the questions choices from names to labels for select_one and select_multiple questions.

--- R/kobo_label.R
+++ R/kobo_label.R
@@ -9,7 +9,7 @@
 #' @param dico ( generated from kobo_dico)
 #'
 #'
-#' @return A "data.table" with the full data.label. To be used for graphs generation.
+#' @return A "data.table" with the full data.label, and choices labels. To be used for graphs generation.
 #'
 #' @author Edouard Legoupil
 #'
@@ -33,7 +33,19 @@ kobo_label <- function(datalabel, dico) {
   names(data.label)[1] <- "fullname"
   data.label <- join (x=data.label, y=dico, by="fullname", type="left" )
   # write.csv(data.label, "out/datalabel.csv")
-  for (i in 1:nrow(data.label)) { attributes(datalabel)$variable.labels[ i] <- as.character(data.label[ i, c("label")]) }
+  for (i in 1:nrow(data.label)) {
+    attributes(datalabel)$variable.labels[ i] <- as.character(data.label[ i, c("label")])
+    
+    if(data.label$type[i] %in% c("select_one", "select_multiple_d")){
+      variablename <- data.label$fullname[i]
+      variableLabel <- as.character(data.label[ i, c("label")])
+      listName <- as.character(data.label[data.label$fullname == variablename, "listname"])
+      choicesLabel <- dico[dico$listname == listName & dico$formpart == "answers", "label"]
+      choicesLabel <- unlist(gsub(pattern = paste0(variableLabel, ": "), "", choicesLabel))
+      choicesName <- unlist(dico[dico$listname == listName & dico$formpart == "answers", "name"])
+      datalabel[[i]] <- mapvalues(datalabel[[i]], from = choicesName, to = choicesLabel, warn_missing = FALSE)
+    }
+  }
   test <- data.label[ !(is.na(data.label$name)), ]
   if (nrow(data.label) > nrow(test)) {
     cat (paste0("you have ",nrow(data.label), " variables in you frame but only ",nrow(test) ," were relabeled.\n"))

83e7cb00f37a96a4b2a5890abfde3132a429b576
ElliottMess <elliott.messeiller@gmail.com>
2019-01-28 13:06:17 -0600

    Adding a dummy dataset and form

---
+++ R/data_koboloadeR.R
@@ -0,0 +1,6 @@
+#' A raw household-level dataset with multiple type of questions
+#' 
+#' @format A data frame with 2407 rows and 128 variables corresponding to a relatively classic data collection ready XLSform. The form and Excel data set can be found in the "Code" folder.
+#' @source \url{http://www.reachresourcecentre.info/system/files/resource-documents/afg_dataset_emergency_response_mechanism_heat_august_2017_to_april_2018.xlsx}
+
+"data_koboloadeR"
